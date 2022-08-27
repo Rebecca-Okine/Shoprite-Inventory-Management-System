b73e2d65ff91c5c428b9b3ad35c7393da2b621a5
Naa <rnaokine002@st.ug.edu.gh>
2022-08-27 23:56:04 +0000

    Update1
    Login form,User Module and Main forms have been created

---
+++ Shoprite Inventory Management System/Shoprite Inventory Management System/Loginform.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Shoprite_Inventory_Management_System
+{
+    partial class Loginform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Loginform));
+            this.label1username = new System.Windows.Forms.Label();
+            this.label2password = new System.Windows.Forms.Label();
+            this.textpassword = new System.Windows.Forms.TextBox();
+            this.button1login = new System.Windows.Forms.Button();
+            this.labelheading = new System.Windows.Forms.Label();
+            this.textbox1username = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelsubheading = new System.Windows.Forms.Label();
+            this.pictureBoxclose = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxclose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1username
+            // 
+            this.label1username.AutoSize = true;
+            this.label1username.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1username.Location = new System.Drawing.Point(106, 187);
+            this.label1username.Name = "label1username";
+            this.label1username.Size = new System.Drawing.Size(68, 14);
+            this.label1username.TabIndex = 2;
+            this.label1username.Text = "Username";
+            this.label1username.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2password
+            // 
+            this.label2password.AutoSize = true;
+            this.label2password.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2password.Location = new System.Drawing.Point(106, 249);
+            this.label2password.Name = "label2password";
+            this.label2password.Size = new System.Drawing.Size(65, 14);
+            this.label2password.TabIndex = 4;
+            this.label2password.Text = "Password";
+            // 
+            // textpassword
+            // 
+            this.textpassword.BackColor = System.Drawing.Color.White;
+            this.textpassword.Font = new System.Drawing.Font("Monotype Corsiva", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textpassword.Location = new System.Drawing.Point(109, 265);
+            this.textpassword.Name = "textpassword";
+            this.textpassword.Size = new System.Drawing.Size(227, 22);
+            this.textpassword.TabIndex = 3;
+            // 
+            // button1login
+            // 
+            this.button1login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(68)))), ((int)(((byte)(255)))));
+            this.button1login.FlatAppearance.BorderColor = System.Drawing.Color.Purple;
+            this.button1login.FlatAppearance.BorderSize = 0;
+            this.button1login.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1login.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1login.ForeColor = System.Drawing.Color.Black;
+            this.button1login.Location = new System.Drawing.Point(109, 334);
+            this.button1login.Name = "button1login";
+            this.button1login.Size = new System.Drawing.Size(227, 38);
+            this.button1login.TabIndex = 5;
+            this.button1login.Text = "LOGIN";
+            this.button1login.UseVisualStyleBackColor = false;
+            // 
+            // labelheading
+            // 
+            this.labelheading.AutoSize = true;
+            this.labelheading.Font = new System.Drawing.Font("Lucida Calligraphy", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelheading.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(68)))), ((int)(((byte)(255)))));
+            this.labelheading.Location = new System.Drawing.Point(127, 101);
+            this.labelheading.Name = "labelheading";
+            this.labelheading.Size = new System.Drawing.Size(172, 24);
+            this.labelheading.TabIndex = 7;
+            this.labelheading.Text = "Welcome Back!";
+            this.labelheading.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textbox1username
+            // 
+            this.textbox1username.BackColor = System.Drawing.Color.White;
+            this.textbox1username.Font = new System.Drawing.Font("Monotype Corsiva", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textbox1username.Location = new System.Drawing.Point(109, 203);
+            this.textbox1username.Name = "textbox1username";
+            this.textbox1username.Size = new System.Drawing.Size(227, 22);
+            this.textbox1username.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(171, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(79, 54);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelsubheading
+            // 
+            this.labelsubheading.AutoSize = true;
+            this.labelsubheading.BackColor = System.Drawing.Color.White;
+            this.labelsubheading.Font = new System.Drawing.Font("Lucida Calligraphy", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelsubheading.ForeColor = System.Drawing.Color.Black;
+            this.labelsubheading.Location = new System.Drawing.Point(148, 134);
+            this.labelsubheading.Name = "labelsubheading";
+            this.labelsubheading.Size = new System.Drawing.Size(131, 17);
+            this.labelsubheading.TabIndex = 9;
+            this.labelsubheading.Text = "Login to Continue";
+            // 
+            // pictureBoxclose
+            // 
+            this.pictureBoxclose.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxclose.Image")));
+            this.pictureBoxclose.Location = new System.Drawing.Point(401, 0);
+            this.pictureBoxclose.Name = "pictureBoxclose";
+            this.pictureBoxclose.Size = new System.Drawing.Size(51, 34);
+            this.pictureBoxclose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxclose.TabIndex = 10;
+            this.pictureBoxclose.TabStop = false;
+            this.pictureBoxclose.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(109, 293);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(102, 17);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Show Password";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Algerian", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(68)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(-5, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(444, 21);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Shoprite Inventory Management System";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Loginform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(451, 470);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBoxclose);
+            this.Controls.Add(this.labelsubheading);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelheading);
+            this.Controls.Add(this.button1login);
+            this.Controls.Add(this.label2password);
+            this.Controls.Add(this.textpassword);
+            this.Controls.Add(this.label1username);
+            this.Controls.Add(this.textbox1username);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Loginform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Loginform_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxclose)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1username;
+        private System.Windows.Forms.Label label2password;
+        private System.Windows.Forms.TextBox textpassword;
+        private System.Windows.Forms.Button button1login;
+        private System.Windows.Forms.Label labelheading;
+        private System.Windows.Forms.TextBox textbox1username;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelsubheading;
+        private System.Windows.Forms.PictureBox pictureBoxclose;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Shoprite Inventory Management System/Shoprite Inventory Management System/UserForm.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Shoprite_Inventory_Management_System
+{
+    partial class UserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridViewuser = new System.Windows.Forms.DataGridView();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.customerButton1 = new Shoprite_Inventory_Management_System.CustomerButton();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewuser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerButton1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkCyan;
+            this.panel1.Controls.Add(this.customerButton1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 504);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1004, 55);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridViewuser
+            // 
+            this.dataGridViewuser.AllowUserToAddRows = false;
+            this.dataGridViewuser.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.DarkCyan;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Cyan;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewuser.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewuser.ColumnHeadersHeight = 30;
+            this.dataGridViewuser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridViewuser.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column7,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridViewuser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewuser.EnableHeadersVisualStyles = false;
+            this.dataGridViewuser.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewuser.Name = "dataGridViewuser";
+            this.dataGridViewuser.Size = new System.Drawing.Size(1004, 504);
+            this.dataGridViewuser.TabIndex = 1;
+            // 
+            // Column7
+            // 
+            this.Column7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column7.HeaderText = "No.";
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 50;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column1.HeaderText = "Username";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 87;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Full name";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column3.HeaderText = "Password";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 84;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column4.HeaderText = "Phone";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 66;
+            // 
+            // Column5
+            // 
+            this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column5.HeaderText = "";
+            this.Column5.Image = ((System.Drawing.Image)(resources.GetObject("Column5.Image")));
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 5;
+            // 
+            // Column6
+            // 
+            this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column6.HeaderText = "";
+            this.Column6.Image = ((System.Drawing.Image)(resources.GetObject("Column6.Image")));
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Manage Users";
+            // 
+            // customerButton1
+            // 
+            this.customerButton1.Image = ((System.Drawing.Image)(resources.GetObject("customerButton1.Image")));
+            this.customerButton1.ImageHover = null;
+            this.customerButton1.ImageNormal = ((System.Drawing.Image)(resources.GetObject("customerButton1.ImageNormal")));
+            this.customerButton1.Location = new System.Drawing.Point(949, 10);
+            this.customerButton1.Name = "customerButton1";
+            this.customerButton1.Size = new System.Drawing.Size(43, 33);
+            this.customerButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.customerButton1.TabIndex = 15;
+            this.customerButton1.TabStop = false;
+            // 
+            // UserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1004, 559);
+            this.Controls.Add(this.dataGridViewuser);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "UserForm";
+            this.Text = "UserForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewuser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerButton1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridViewuser;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewImageColumn Column5;
+        private System.Windows.Forms.DataGridViewImageColumn Column6;
+        private CustomerButton customerButton1;
+    }
+}
